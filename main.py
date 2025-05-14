ac56429e88aadd23767c32f9089b4c9895dc733a
marolive <marolive@redhat.com>
2025-05-13 22:04:14 -0300

    Create, update e delete item

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from item import Item
 
 app = FastAPI(
     title="My FastAPI API",
@@ -40,3 +41,22 @@ items = []
 @app.get("/items")
 async def get_items():
     return items
+
+@app.post("/items", status_code=201)
+async def create_item(item: Item):
+    items.append(item.dict())
+    return item
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    if 0 <= item_id < len(items):
+        items[item_id].update(item.dict())
+        return items[item_id]
+    raise HTTPException(status_code=404, detail="Item não encontrado")
+
+@app.delete("/items/{item_id}")
+async def delete_item(item_id: int):
+    if 0 <= item_id < len(items):
+        removed_item = items.pop(item_id)
+        return removed_item
+    raise HTTPException(status_code=404, detail="Item não encontrado")
